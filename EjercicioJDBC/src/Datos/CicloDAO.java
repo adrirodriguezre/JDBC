b7b4772569e6c5acec48241344623dcd1ccb73d0
adrirodriguezre <59870702+adrirodriguezre@users.noreply.github.com>
2020-12-11 14:33:50 +0100

    Add files via upload

---
+++ EjercicioJDBC/src/Datos/CicloDAO.java
@@ -0,0 +1,34 @@
+package Datos;
+
+import Entidades.Ciclo;
+import Entidades.ICicloDAO;
+
+public class CicloDAO implements ICicloDAO {
+
+	@Override
+	public void insertar(Ciclo ciclo) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+	@Override
+	public void eliminar(Ciclo ciclo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificar(Ciclo ciclo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void crearCiclo(Ciclo ciclo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ EjercicioJDBC/src/Entidades/IAsignaturaDAO.java
@@ -0,0 +1,9 @@
+package Entidades;
+
+public interface IAsignaturaDAO {
+
+	public void insertar (Asignatura asignatura);
+	public void eliminar (Asignatura asignatura);
+	public void modificar (Asignatura asignatura);
+	
+}
